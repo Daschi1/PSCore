a45fa8413bde436f124eb135d40f2b4ae0a71a4d
Restyled.io <commits@restyled.io>
2020-07-05 16:18:07 +0000

    Restyled by clang-format

--- src/main/java/de/daschi/pscore/PSCore.java
+++ src/main/java/de/daschi/pscore/PSCore.java
@@ -2,44 +2,41 @@ package de.daschi.pscore;
 
 import de.daschi.pscore.config.CoreConfig;
 import de.daschi.pscore.mysql.CoreMySQL;
+import java.io.IOException;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.simpleyaml.exceptions.InvalidConfigurationException;
 
-import java.io.IOException;
-
 public final class PSCore extends JavaPlugin {
 
-    private static JavaPlugin javaPlugin;
+  private static JavaPlugin javaPlugin;
 
-    public static String getVersion() {
-        return PSCore.javaPlugin.getDescription().getVersion();
+  public static String getVersion() {
+    return PSCore.javaPlugin.getDescription().getVersion();
+  }
+
+  public static JavaPlugin getJavaPlugin() { return PSCore.javaPlugin; }
+
+  @Override
+  public void onLoad() {
+    super.onLoad();
+    PSCore.javaPlugin = this;
+
+    try {
+      CoreConfig.loadConfig();
+    } catch (final IOException | InvalidConfigurationException e) {
+      e.printStackTrace();
     }
 
-    public static JavaPlugin getJavaPlugin() {
-        return PSCore.javaPlugin;
-    }
+    CoreMySQL.loadMySQL();
+  }
 
-    @Override
-    public void onLoad() {
-        super.onLoad();
-        PSCore.javaPlugin = this;
+  @Override
+  public void onEnable() {
+    super.onEnable();
+  }
 
-        try {
-            CoreConfig.loadConfig();
-        } catch (final IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
-
-        CoreMySQL.loadMySQL();
-    }
-
-    @Override
-    public void onEnable() {
-        super.onEnable();
-    }
-
-    @Override
-    public void onDisable() {
-        super.onDisable();
-    }
+  @Override
+  public void onDisable() {
+    super.onDisable();
+  }
 }
