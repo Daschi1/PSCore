a0ad6e3a7005a1b610f540c47472daad2cc5147d
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2020-07-06 21:25:05 +0200

    Added ps stuff

---
+++ src/main/java/de/daschi/pscore/ps/PSMain.java
@@ -0,0 +1,41 @@
+package de.daschi.pscore.ps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class PSMain extends JavaPlugin {
+
+    private static JavaPlugin javaPlugin;
+
+    public static JavaPlugin getJavaPlugin() {
+        return PSMain.javaPlugin;
+    }
+
+    public abstract void load();
+
+    public abstract void enable();
+
+    public abstract void disable();
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        PSMain.javaPlugin = this;
+
+        this.load();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+        this.enable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+        this.disable();
+    }
+}
